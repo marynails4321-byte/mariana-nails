94857ef88b2ec14c6b69bac2bd7c117953e0d4da
RenTcar <RenTcarCop@gmail.com>
2026-09-08 18:11:07 -0500

    Primer commit: Estructura inicial de Mariana Nails Studio

--- admin.php
+++ admin.php
@@ -138,7 +138,7 @@ try {
     <title>Agenda & Panel - Mariana Nails Studio</title>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,500;0,600;1,400&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="admin.css">
 </head>
 <body class="login-body agenda-body-align">
 
