a3a671bfe367d6883e59fb93380ea58f76072189
RenTcar <RenTcarCop@gmail.com>
2026-09-08 19:50:55 -0500

    imagenes

--- agendar.php
+++ agendar.php
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <form action="agendar.php" method="POST">
             
-            <label class="form-label-luxury">1. Selecciona el Diseño o Servicio:</label>
+            <label class="form-label-luxury">1. Selecciona el Servicio:</label>
             <div class="services-grid">
                 
                 <?php if (empty($servicios_db)): ?>
